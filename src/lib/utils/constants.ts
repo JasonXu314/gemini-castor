6a5d10303158b3ddf45a21f7e7123323ffaed663
Jason Xu <Xinwen31415@gmail.com>
2021-08-03 17:12:14 -0500

    fixed for deploy

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,5 +1,5 @@
 // FIXME: CHANGE THIS VARIABLE WHEN DEPLOYING (not a fixme)
 // https://gemini-backnd.herokuapp.com
 // http://localhost:5000
-// export const BACKEND_URL = 'https://gemini-backnd.herokuapp.com';
-export const BACKEND_URL = 'http://localhost:5000';
+export const BACKEND_URL = 'https://gemini-backnd.herokuapp.com';
+// export const BACKEND_URL = 'http://localhost:5000';
